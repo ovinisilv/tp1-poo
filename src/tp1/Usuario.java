b59a0c6522e4bb6c0964651afe686cfa2fa15257
Vinicius Silva <viniguerras@hotmail.com>
2026-04-28 19:09:32 -0300

    Update Usuario.java

--- src/tp1/Usuario.java
+++ src/tp1/Usuario.java
@@ -1,49 +1,25 @@
-package tp1;
-
-
-public class Usuario{
+public class Usuario {
     private String nome;
     private int idade;
     private String filme_alugado;
 
-    public Usuario(String nome, int idade){
+    public Usuario(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
         this.filme_alugado = null;
     }
 
-    public String getNome() {
-        return nome;
+    public void setFilme_alugado(String filme) {
+        this.filme_alugado = filme;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getFilme_alocado() {
+    public String getFilme_alugado() {
         return filme_alugado;
     }
 
-    public void setFilme_alugado(String filme_alugado) {
-        this.filme_alugado = filme_alugado;
-    }
-    
-    public void monstrar_usuario(){
+    public void mostrarUsuario() {
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
-        if(filme_alugado != null){
-            System.out.println("Filme: " + filme_alugado);
-        } else{
-            System.out.println("Nenhum filme alugado.");
-        }
+        System.out.println("Filme alugado: " + (filme_alugado != null ? filme_alugado : "Nenhum"));
     }
 }
-
